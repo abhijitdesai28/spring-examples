ad5c2d67cf52819d9fa4b66e5ea28c862ca414dd
unknown <desaiabhijit28@gmail.com>
2024-03-28 20:51:03 +0530

    spring mvc example

---
+++ spring-mvc-example/src/main/java/com/abhijit/practice/model/User.java
@@ -0,0 +1,17 @@
+package com.abhijit.practice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String username;
+    private String password;
+    private String address;
+    private boolean receivePaper;
+    private String[] favoriteFrameworks;
+    private String gender;
+    private String favoriteNumber;
+    private String country;
+}
