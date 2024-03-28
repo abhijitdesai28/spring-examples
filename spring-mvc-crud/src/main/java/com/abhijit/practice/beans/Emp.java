4a4e833300eeb28b5f6730c4f45f242a01300e76
unknown <desaiabhijit28@gmail.com>
2024-03-28 20:49:38 +0530

    spring mvc crud

---
+++ spring-mvc-crud/src/main/java/com/abhijit/practice/beans/Emp.java
@@ -0,0 +1,13 @@
+package com.abhijit.practice.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Emp {
+    private int id;
+    private String name;
+    private float salary;
+    private String designation;
+}  
